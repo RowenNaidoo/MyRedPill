0a0452135b36f5af5dc5696920597a75531ea541
RowenNaidoo <rowen.naidoo@gmail.com>
2016-04-21 21:42:11 +1000

    Added files via upload

---
+++ Readify.Service/IMyRedPill.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Readify.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyRedPill" in both code and config file together.
+    [ServiceContract]
+    public interface IMyRedPill
+    {
+        [OperationContract]
+        string DoWork();
+
+        [OperationContract]
+        TriangleType test();
+    }
+
+    [DataContract]
+    public enum TriangleType
+    {
+        a=1,
+        b=2,
+        c=3
+    }
+}
---
+++ Readify.Service/MyRedPill.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Readify.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyRedPill" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyRedPill.svc or MyRedPill.svc.cs at the Solution Explorer and start debugging.
+    public class MyRedPill : IMyRedPill
+    {
+        public string DoWork()
+        {
+            return "balls";
+        }
+
+        public TriangleType test()
+        {
+            return new TriangleType();
+        }
+    }
+}
